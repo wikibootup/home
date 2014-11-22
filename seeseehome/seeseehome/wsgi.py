9f2f454fbbfa57f42e5d45cd844ea000ef86b5c0
wikibootup <wikibootup@gmail.com>
2014-11-23 01:30:06 +0900

    startproject : all setting files added & users app added in INTALLED_APPS

---
+++ seeseehome/seeseehome/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for seeseehome project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "seeseehome.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
