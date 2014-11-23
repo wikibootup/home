ba924dcbb447c5591126dfb1fe8b7c8584d8b6b5
wikibootup <wikibootup@gmail.com>
2014-11-23 23:33:36 +0900

    test user manager : create_user, validate password

--- seeseehome/users/tests/test_user_manager.py
+++ seeseehome/users/tests/test_user_manager.py
@@ -1,9 +1,51 @@
 from django.test import TestCase
-from users.models import User, UserManager
+from users.models import User
+from django.core.exceptions import ObjectDoesNotExist,ValidationError
+from seeseehome import msg
 
 class UserManagerTestCase(TestCase):
     def setUp(self):
-        pass
+        pass    
 
-    def test_test(self):
-        pass
+    def test_create_user(self):
+        print (msg.users_valid_password)
+        self.assertIsNotNone(
+            User.objects.create_user(
+                username = msg.users_valid_name,
+                email = msg.users_valid_email,
+                password = msg.users_valid_password,
+            )
+        )
+
+    def test_get_user(self):
+        user = User.objects.create_user(
+                   username = msg.users_valid_name,
+                   email = msg.users_valid_email,
+                   password = msg.users_valid_password,
+               )
+                                
+        self.assertIsNotNone(
+            User.objects.get_user(
+                id = user.id
+            )
+        )
+
+    def test_user_password_without_alphabet_char(self):
+        self.assertRaises(
+            ValidationError,
+            User.objects.create_user,
+            username = msg.users_valid_name,
+            email = msg.users_valid_email,
+            password = msg.users_pwd_without_alphabet_char,
+        )
+
+    def test_user_password_without_special_char(self):
+        self.assertRaises(
+            ValidationError,
+            User.objects.create_user,
+            username = msg.users_valid_name,
+            email = msg.users_valid_email,
+            password = msg.users_pwd_without_special_char,
+        )
+
+
