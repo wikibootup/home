ea0bbb7495e0e71217500ff8ea530b1ca21fdc28
wikibootup <wikibootup@gmail.com>
2014-11-24 00:48:25 +0900

    messages added

--- seeseehome/seeseehome/msg.py
+++ seeseehome/seeseehome/msg.py
@@ -12,3 +12,11 @@ users_pwd_no_special_char = "user password must contain special character"
 ### username
 users_name_must_be_set = "The given username must be set"
 users_name_at_most_30 = "user name max length is 30"
+users_invalid_name = "user name cannot contain special characters" + \
+                    "except for under bar_ , hyphen -"
+
+### update user
+users_update_without_any_required_fields = \
+    "possible update field : username, email"
+
+
