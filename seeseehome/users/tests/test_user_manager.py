af20239f193ebdfaca404e0914f6ba315c26d4f4
wikibootup <wikibootup@gmail.com>
2014-11-23 23:41:28 +0900

    test user manager : seperation to testdata.py

--- seeseehome/users/tests/test_user_manager.py
+++ seeseehome/users/tests/test_user_manager.py
@@ -1,27 +1,27 @@
 from django.test import TestCase
 from users.models import User
 from django.core.exceptions import ObjectDoesNotExist,ValidationError
-from seeseehome import msg
+from seeseehome import testdata
 
 class UserManagerTestCase(TestCase):
     def setUp(self):
         pass    
 
     def test_create_user(self):
-        print (msg.users_valid_password)
+        print (testdata.users_valid_password)
         self.assertIsNotNone(
             User.objects.create_user(
-                username = msg.users_valid_name,
-                email = msg.users_valid_email,
-                password = msg.users_valid_password,
+                username = testdata.users_valid_name,
+                email = testdata.users_valid_email,
+                password = testdata.users_valid_password,
             )
         )
 
     def test_get_user(self):
         user = User.objects.create_user(
-                   username = msg.users_valid_name,
-                   email = msg.users_valid_email,
-                   password = msg.users_valid_password,
+                   username = testdata.users_valid_name,
+                   email = testdata.users_valid_email,
+                   password = testdata.users_valid_password,
                )
                                 
         self.assertIsNotNone(
@@ -34,18 +34,18 @@ class UserManagerTestCase(TestCase):
         self.assertRaises(
             ValidationError,
             User.objects.create_user,
-            username = msg.users_valid_name,
-            email = msg.users_valid_email,
-            password = msg.users_pwd_without_alphabet_char,
+            username = testdata.users_valid_name,
+            email = testdata.users_valid_email,
+            password = testdata.users_pwd_without_alphabet_char,
         )
 
     def test_user_password_without_special_char(self):
         self.assertRaises(
             ValidationError,
             User.objects.create_user,
-            username = msg.users_valid_name,
-            email = msg.users_valid_email,
-            password = msg.users_pwd_without_special_char,
+            username = testdata.users_valid_name,
+            email = testdata.users_valid_email,
+            password = testdata.users_pwd_without_special_char,
         )
 
 
