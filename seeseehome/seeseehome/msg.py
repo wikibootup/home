12d2a86597551eec7048c42012ef0c8162a169bc
wikibootup <wikibootup@gmail.com>
2014-11-23 23:40:48 +0900

    msg.py : seperation to testdata.py

--- seeseehome/seeseehome/msg.py
+++ seeseehome/seeseehome/msg.py
@@ -2,26 +2,6 @@
 
 
 #### users
-### test data
-users_valid_email = "test@example.com"
-users_second_valid_email = "secondtest@example.com"
-users_superuser_email = "superuser@example.com"
-users_valid_password = "test_password123!"
-users_pwd_without_special_char = "test_password123"
-users_pwd_without_alphabet_char = "123123!"
-users_not_created_user_email = "nouser@example.com"
-users_not_created_user_id = 0
-users_old_name = "OldName1234"
-users_new_name = "NewName1234"
-users_name_with_special_char = "NewName1234!@#$"
-users_new_over_length_name = "a" * 31
-
-# The following email is not sign-in yet
-users_available_email = "available_email@example.com"
-users_valid_name = "Available_-123User-Name"
-users_invalid_name_exceed_the_maximum = "a" * 31
-users_invalid_name = "invalid_name!@#"
-
 ### password
 users_pwd_at_least_6 = "user password length should be at least 6"
 users_pwd_at_most_255 = "user password max length is 255"
