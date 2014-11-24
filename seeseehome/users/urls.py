b5bfa568ace1ae7052671b8b85207872f5cca185
wikibootup <wikibootup@gmail.com>
2014-11-25 04:35:34 +0900

    logout url added

--- seeseehome/users/urls.py
+++ seeseehome/users/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
-from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
     url(r'^signup/', 'users.views.signup', name="signup"),
     url(r'^login/', 'users.views.login', name="login"),
+    url(r'^logout/', 'users.views.logout', name="logout"),
 )
 
