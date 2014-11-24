3b3082ef44531c6852136fa308d905f478cdf9fc
wikibootup <wikibootup@gmail.com>
2014-11-24 20:36:26 +0900

    user view : redirect method added

--- seeseehome/users/views.py
+++ seeseehome/users/views.py
@@ -5,19 +5,25 @@ from seeseehome import msg
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.contrib import messages
 from django.core.validators import validate_email
+from django.core.urlresolvers import reverse
+
+def delete_dicts(dicts):
+    for dict_var in dicts:
+        del dicts[dict_var]
 
 def signup(request):
-    if request.method == 'POST':    
-        ### username
+    ### username
+    if request.method == 'POST':
         username = request.POST['username']
-
         ## username validator
         try:
             User.objects.validate_username(username)
         except ValidationError:
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_invalid_name)
-
+            delete_dicts(request.POST)
+            return HttpResponseRedirect(reverse("users:signup"))
+       
         ####
         ## username unique check
         try:
@@ -27,6 +33,7 @@ def signup(request):
         else:
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_exist_name)
+            return HttpResponseRedirect(reverse("users:signup"))
             
         ######
         ### email
@@ -36,7 +43,8 @@ def signup(request):
         except ValidationError:
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_invalid_email)
-
+            return HttpResponseRedirect(reverse("users:signup"))
+       
         ####
         ## email unique check
         try:
@@ -46,22 +54,31 @@ def signup(request):
         else:
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_exist_email)
-
+            return HttpResponseRedirect(reverse("users:signup"))
+      
         ######
         ### password
         password = request.POST['pwd']
+        password_confirmation = request.POST['confirm_pwd']
+    
+        if password != password_confirmation:
+            pass
+    
         try:
             User.objects.validate_password(password)
         except ValidationError:
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_invalid_pwd)
+            return HttpResponseRedirect(reverse("users:signup"))
+              
         ######
         ### User Registration
         User.objects.create_user(username = username, email = email,
                                 password = password)
-
+        
         messages.success(request, msg.users_signup_success)
         messages.info(request, msg.users_signup_success_info)
+        return HttpResponseRedirect(reverse("users:signup"))
 
     return render(
                request,
