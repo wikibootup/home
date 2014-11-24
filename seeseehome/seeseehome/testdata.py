fe09f22cb8c897b66efed108e2a9d781d4fbcd1f
wikibootup <wikibootup@gmail.com>
2014-11-24 16:32:46 +0900

    test data added

--- seeseehome/seeseehome/testdata.py
+++ seeseehome/seeseehome/testdata.py
@@ -4,6 +4,7 @@
 #### users
 ### test data
 users_valid_email = "test@example.com"
+users_available_email = "available_email@example.com"
 users_second_valid_email = "secondtest@example.com"
 users_superuser_email = "superuser@example.com"
 users_valid_password = "test_password123!"
@@ -20,7 +21,6 @@ users_new_name = "NewName1234-_"
 users_name_with_special_char = "NewName1234!@#$"
 users_new_over_length_name = "a" * 31
 # The following email is not sign-in yet
-users_available_email = "available_email@example.com"
 users_valid_name = "Available_-123User-Name"
 users_invalid_name_exceed_the_maximum = "a" * 31
 users_name_with_special_char = "invalid_name!@#"
