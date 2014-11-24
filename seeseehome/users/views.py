8c8317ed13930dc2f9538c126ac839750452e9c3
wikibootup <wikibootup@gmail.com>
2014-11-24 23:57:52 +0900

    user view : login view, signup view added

--- seeseehome/users/views.py
+++ seeseehome/users/views.py
@@ -1,16 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, request
+from django.http import HttpResponse, HttpResponseRedirect, request
 from users.models import User
 from seeseehome import msg
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.contrib import messages
 from django.core.validators import validate_email
 from django.core.urlresolvers import reverse
+from django.contrib.auth import login as auth_login
+
+# I couldn't solve built-in authenticate problem yet
+# So I use custom authenticate(But It is same as built-in authenticate)
+def authenticate(username=None, password=None):
+    try:
+        user = User.objects.get(username=username)
+        if user.check_password(password):
+            return user
+    except User.DoesNotExist:
+        return None
 
 def delete_dicts(dicts):
     for dict_var in dicts:
         del dicts[dict_var]
 
+def login(request):
+    ### username
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pwd']
+        user = authenticate(username = username, password = password)
+        if user is not None:
+            user.backend = 'django.contrib.auth.backends.ModelBackend'
+            if user.is_active:
+                auth_login(request, user)
+                messages.success(request, msg.users_login_success)
+                messages.info(request, msg.users_login_success_info)
+    
+                next = ""
+                if 'next' in request.GET:
+                    next = request.GET['next']
+
+                if next == "":
+                    return HttpResponseRedirect(reverse("home"))
+                else:
+                    return HttpResponseRedirect(next)
+        else:
+            messages.error(request, msg.users_login_error)
+            messages.info(request, msg.users_invalid)
+            return HttpResponseRedirect(reverse("users:login"))
+ 
+    return render(request, "users/login.html")
+ 
 def signup(request):
     ### username
     if request.method == 'POST':
@@ -19,11 +58,11 @@ def signup(request):
         try:
             User.objects.validate_username(username)
         except ValidationError:
-            messages.error(request, msg.users_signup_error)
-            messages.info(request, msg.users_invalid_name)
-            delete_dicts(request.POST)
-            return HttpResponseRedirect(reverse("users:signup"))
-       
+          message.error(request, msg.users_signup_error)
+          messages.info(request, msg.users_invalid_name)
+          delete_dicts(request.POST)
+          return HttpResponseRedirect(reverse("users:signup"))
+        
         ####
         ## username unique check
         try:
@@ -44,7 +83,7 @@ def signup(request):
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_invalid_email)
             return HttpResponseRedirect(reverse("users:signup"))
-       
+        
         ####
         ## email unique check
         try:
@@ -55,15 +94,17 @@ def signup(request):
             messages.error(request, msg.users_signup_error)
             messages.info(request, msg.users_exist_email)
             return HttpResponseRedirect(reverse("users:signup"))
-      
+        
         ######
         ### password
         password = request.POST['pwd']
         password_confirmation = request.POST['confirm_pwd']
-    
+        
         if password != password_confirmation:
-            pass
-    
+            messages.error(request, msg.users_signup_error)
+            messages.info(request, msg.users_confirm_pwd_error)
+            return HttpResponseRedirect(reverse("users:signup"))
+
         try:
             User.objects.validate_password(password)
         except ValidationError:
@@ -78,10 +119,7 @@ def signup(request):
         
         messages.success(request, msg.users_signup_success)
         messages.info(request, msg.users_signup_success_info)
-        return HttpResponseRedirect(reverse("users:signup"))
+        return HttpResponseRedirect(reverse("users:login"))
 
-    return render(
-               request,
-               "users/signup.html",
-           )
+    return render(request, "users/signup.html")
 
