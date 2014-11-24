e266fb2aa3334a0f06015d22cfa0de6ced26c7a4
wikibootup <wikibootup@gmail.com>
2014-11-24 16:32:36 +0900

    messages added

--- seeseehome/seeseehome/msg.py
+++ seeseehome/seeseehome/msg.py
@@ -11,12 +11,30 @@ users_pwd_no_special_char = "user password must contain special character"
 
 ### username
 users_name_must_be_set = "The given username must be set"
-users_name_at_most_30 = "user name max length is 30"
+users_name_at_most_30 = "user name length is limited to 30"
 users_invalid_name = "user name cannot contain special characters" + \
                     "except for under bar_ , hyphen -"
 
 ### update user
 users_update_without_any_required_fields = \
     "possible update field : username, email"
+
+
+### signup
+## signup
+users_signup_error = "ERROR : Sign Up"
+users_signup_success = "SUCCESS : Sign Up"
+users_signup_success_info = "You can login now"
+users_exist_email = "The user email already exist"
+users_invalid_email = \
+    "Invalid Email.\n" + \
+    "It must have user part and domain part between '@'\n" + \
+    "Or It might contain invalid special character"
+users_exist_name = "The user name already exist"
+users_invalid_pwd = \
+    "Invalid password.\n" + \
+    "password length is in range 6 ~ 255\n" + \
+    "And it should contain numeric character, alphabet character," + \
+    "and special character"
 
 
