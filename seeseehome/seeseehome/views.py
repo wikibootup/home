7614c6141acbfb9f855e038b40d56474598061c1
wikibootup <wikibootup@gmail.com>
2014-11-25 04:36:48 +0900

    home view : CBV -> FBV

--- seeseehome/seeseehome/views.py
+++ seeseehome/seeseehome/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
 
-class Home(TemplateView):
-    template_name = "home.html"
+def home(request):
+    return render(request, "home.html")
