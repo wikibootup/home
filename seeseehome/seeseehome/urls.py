ce75194c331504536b72c95112e9a606a3c3a251
wikibootup <wikibootup@gmail.com>
2014-11-24 23:56:41 +0900

    main page added (login required)

--- seeseehome/seeseehome/urls.py
+++ seeseehome/seeseehome/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from seeseehome.views import Home
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', login_required(Home.as_view()), name='home'),
 
     # users' urls
     url(r'^', include('users.urls', namespace='users')),
