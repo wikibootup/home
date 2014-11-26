2d0edb4a9d466dcd0bbbcc6f8efb64b5bce43717
wikibootup <wikibootup@gmail.com>
2014-11-26 14:08:07 +0900

    url boardlist -> boards, but url name is still boardlist

--- seeseehome/boards/urls.py
+++ seeseehome/boards/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^boardlist/', 'boards.views.boardlist', name="boardlist"),
+    url(r'^boards/$', 'boards.views.boardlist', name="boardlist"),
     url(r'^boards/([0-9]+)/$', 'boards.views.boardpage', name="boardpage"), 
     url(r'^boards/([0-9]+)/posts/([0-9]+)/$', 'boards.views.postpage', 
         name="postpage"),
