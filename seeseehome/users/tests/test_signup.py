4c6e5d43e012f2d545ae3ecd411cd4555522cd33
wikibootup <wikibootup@gmail.com>
2014-11-26 13:54:29 +0900

    test signup : url -> url name

--- seeseehome/users/tests/test_signup.py
+++ seeseehome/users/tests/test_signup.py
@@ -3,20 +3,22 @@ from users.models import User
 from django.core.exceptions import ObjectDoesNotExist,ValidationError
 from seeseehome import testdata, msg
 from django.test.client import Client
+from django.core.urlresolvers import reverse
 
 class UserSignUpTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
     def test_get_response_to_signup_page(self):
-        response = self.client.get("/signup/", follow = True)
-        self.assertEqual(response.wsgi_request.path, "/signup/")
+        response = self.client.get(reverse("users:signup"), follow = True)
+        self.assertEqual(response.wsgi_request.path, reverse("users:signup"))
     """
     # signup test method has a problem.
     def test_signup(self):
         response = \
             self.client.post(
-                "/signup/", {
+                reverse("users:signup"), 
+                {
                     "uesrname" : testdata.users_valid_name, 
                     "email" : testdata.users_valid_email,
                     "pwd" : testdata.users_valid_password,
