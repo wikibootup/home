ec1d7e9af2b7a9ba8b3c18891ef07ac4cbf6220c
wikibootup <wikibootup@gmail.com>
2014-11-29 23:30:19 +0900

    boards/views.py : paginator methd applied

--- seeseehome/boards/views.py
+++ seeseehome/boards/views.py
@@ -8,6 +8,7 @@ from django.core.urlresolvers import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 #from boards.forms import WriteForm
+from django.core.paginator import Paginator
 
 @login_required
 def write(request, board_id, **extra_fields):
@@ -81,26 +82,56 @@ def write(request, board_id, **extra_fields):
         messages.success(request, msg.boards_write_success)
         messages.info(request, msg.boards_write_success_info)
 
-        boardposts = BoardPosts.objects.filter(board=board)
+        boardposts = \
+            (BoardPosts.objects.filter(board=board)).order_by('-posted_date')
+        custom_paginator = pagination(
+                               boardposts=boardposts, 
+                               posts_per_page = 10
+                           )
         return render(request, "boards/boardpage.html", 
-                {'board_id' : board_id, 'boardposts' : boardposts})
+               {
+                   'board_id' : board_id,
+                   'boardposts' : custom_paginator['boardposts'],
+                   'paginator' :custom_paginator['paginator'],
+                   'has_next' : custom_paginator['has_next'],
+                   'has_previous' : custom_paginator['has_previous'],
+                   'nextpage' : custom_paginator['nextpage'],
+                   'previous_page' : custom_paginator['previous_page'],
+               }
+           )
 
     return render(request, "boards/write.html")
 
 #@login_required
 def rewrite(request, board_id, post_id):
     board = Board.objects.get_board(board_id)
+    boardposts = BoardPosts.objects.filter(board=board)
     post = Post.objects.get_post(post_id) 
     
     if request.method == 'POST':
         write(request, board_id, post_id=post.id)
-        boardposts = BoardPosts.objects.filter(board=board)
+        boardposts = \
+            (BoardPosts.objects.filter(board=board)).order_by('-posted_date')
+        custom_paginator = pagination(
+                               boardposts=boardposts, 
+                               posts_per_page = 10
+                           )
+
         return render(request, "boards/boardpage.html", 
-                {'board_id' : board_id, 'boardposts' : boardposts})
+               {
+                   'board_id' : board_id,
+                   'boardposts' : custom_paginator['boardposts'],
+                   'paginator' :custom_paginator['paginator'],
+                   'has_next' : custom_paginator['has_next'],
+                   'has_previous' : custom_paginator['has_previous'],
+                   'nextpage' : custom_paginator['nextpage'],
+                   'previous_page' : custom_paginator['previous_page'],
+               }
+           )
+
 
     return render(request, "boards/rewrite.html",
             {'board' : board, 'post' : post})
-
 
 def postpage(request, board_id, post_id):
     board = Board.objects.get_board(board_id)
@@ -108,11 +139,53 @@ def postpage(request, board_id, post_id):
     return render(request, "boards/postpage.html",
             {'board' : board, 'post' : post})
 
-def boardpage(request, board_id):
+def pagination(boardposts, posts_per_page, page=1):
+#   posts per page
+    start_pos = (int(page)-1) * posts_per_page
+    end_pos = start_pos + posts_per_page
+    boardposts_per_page = boardposts[start_pos : end_pos]
+    paginator = Paginator(boardposts, posts_per_page).page(page)
+    has_next = paginator.has_next()
+    has_previous = paginator.has_previous()
+    nextpage = page
+    previous_page = page
+    if has_next:
+        nextpage = paginator.next_page_number()
+    if has_previous:
+        previous_page = paginator.previous_page_number()
+
+    custom_paginator = {
+                               'boardposts' : boardposts_per_page,
+                               'paginator' : paginator,
+                               'has_next' : has_next,
+                               'has_previous' : has_previous,
+                               'nextpage' : nextpage,
+                               'previous_page' : previous_page,
+                           }
+    return custom_paginator
+
+def boardpage(request, board_id, page):
+#   get board
     board = Board.objects.get_board(board_id)
-    boardposts = BoardPosts.objects.filter(board=board)
+
+
+#   the following line is important to the page list (prev page, next page)
+    boardposts = \
+        (BoardPosts.objects.filter(board=board)).order_by('-posted_date')
+    custom_paginator = pagination(boardposts=boardposts, posts_per_page = 10,
+                        page=page)
+
     return render(request, "boards/boardpage.html", 
-            {'board_id' : board_id, 'boardposts' : boardposts})
+               {
+                   'board_id' : board_id,
+                   'boardposts' : custom_paginator['boardposts'],
+                   'paginator' :custom_paginator['paginator'],
+                   'has_next' : custom_paginator['has_next'],
+                   'has_previous' : custom_paginator['has_previous'],
+                   'nextpage' : custom_paginator['nextpage'],
+                   'previous_page' : custom_paginator['previous_page'],
+               }
+           )
 
 def boardlist(request):
     boardlist = Board.objects.all()
