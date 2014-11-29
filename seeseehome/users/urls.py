9df740d15ff80ed5dc0e0be56c0bb146c7f88082
wikibootup <wikibootup@gmail.com>
2014-11-30 01:47:13 +0900

    users url : url of personal info and password change added

--- seeseehome/users/urls.py
+++ seeseehome/users/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^signup/', 'users.views.signup', name="signup"),
-    url(r'^login/', 'users.views.login', name="login"),
-    url(r'^logout/', 'users.views.logout', name="logout"),
+    url(r'^signup/$', 'users.views.signup', name="signup"),
+    url(r'^login/$', 'users.views.login', name="login"),
+    url(r'^logout/$', 'users.views.logout', name="logout"),
+    url(r'^personalinfo/$', 'users.views.personalinfo', name="personalinfo"),
+    url(r'^editpwd/$', 'users.views.editpassword', name="editpwd"),
 )
 
