12610b42e903ff8264a7bf0ad590cc0f217acc67
wikibootup <wikibootup@gmail.com>
2014-11-29 15:44:47 +0900

    boards form : django-from not used, but template form used

--- seeseehome/boards/forms.py
+++ seeseehome/boards/forms.py
@@ -1,3 +1,4 @@
+"""
 from django import forms
 from ckeditor.fields import RichTextFormField
 from ckeditor.widgets import CKEditorWidget
@@ -7,19 +8,11 @@ class WriteForm(forms.Form):
 
 #   For more Custom Client API, subject form was implemented 
 #   in template(html)
-    """
-    subject = forms.CharField(
-                  max_length = 255,
-                  label="",
-                  widget=forms.TextInput(
-                      attrs={'placeholder': 'Enter Subject'},
-                  ),
-                  required = True,
-              )
-    """
+
+#   This form is not used. It is just for reference 
     content = forms.CharField(
                   label="",
                   widget=CKEditorWidget(),
                   max_length = 65535,
               )
-
+"""
