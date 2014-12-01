ad20c3de99461ed77cdb23073b8cbb41df6cb6b1
wikibootup <wikibootup@gmail.com>
2014-12-02 06:51:36 +0900

    board admin : form added, <add> permission deleted, list display, search fields

--- seeseehome/boards/admin.py
+++ seeseehome/boards/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
-from boards.models import Board
+from boards.models import Board, Post, BoardPosts
+from boards.forms import BoardForm
+
 class BoardAdmin(admin.ModelAdmin):
-    pass
+    form = BoardForm
+    list_display = ('boardname',)
+
+class PostAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    list_display = ('subject', 'writer', 'date_posted',)
+    exclude = ('writer', 'subject', 'content',)
+    search_fields = ['subject']
 
 admin.site.register(Board, BoardAdmin)
-
+admin.site.register(Post, PostAdmin)
 
