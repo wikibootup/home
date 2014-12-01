869941329f969e466f8fc0ef4ee5e303e6a20af7
wikibootup <wikibootup@gmail.com>
2014-12-02 06:52:04 +0900

    board form : validator form for admin page

--- seeseehome/boards/forms.py
+++ seeseehome/boards/forms.py
@@ -16,3 +16,29 @@ class WriteForm(forms.Form):
                   max_length = 65535,
               )
 """
+
+from django import forms
+from boards.models import Board
+from seeseehome import msg
+from django.core.exceptions import ValidationError
+
+class BoardForm(forms.ModelForm):
+    def clean_boardname(self):
+#       More than Max number of boards?        
+        try:
+            Board.objects.validate_max_number_of_boards(
+                Board.objects.all().count()
+            )
+        except ValidationError:
+            raise forms.ValidationError(msg.boards_max_number_of_boards)
+
+#       Board name validator
+        try:
+            Board.objects.validate_boardname(self.cleaned_data["boardname"])
+        except ValueError:
+            raise forms.ValidationError(msg.boards_name_must_be_set)
+        except ValidationError:
+            raise forms.ValidationError(msg.boards_name_at_most_30)
+
+        return self.cleaned_data["boardname"]
+
