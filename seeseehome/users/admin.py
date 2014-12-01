8ce0ab5de6d20c7a6c4d89362c0273088ead1533
wikibootup <wikibootup@gmail.com>
2014-12-01 23:36:04 +0900

    group category in admin page was removed

--- seeseehome/users/admin.py
+++ seeseehome/users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from users.models import User
+from django.contrib.auth.models import Group
+
+admin.site.unregister(Group)
 
 class UserAdmin(admin.ModelAdmin):
     exclude = ('password', 'username', 'email', 
                 'last_login',)
 
 admin.site.register(User, UserAdmin)
-
 
