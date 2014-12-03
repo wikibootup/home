d6ddd2548d22f972311f2b6b794f579af262e4bc
wikibootup <wikibootup@gmail.com>
2014-12-04 03:19:06 +0900

    linkpost url conf added

--- seeseehome/linkboard/urls.py
+++ seeseehome/linkboard/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^linkboard/([0-9]+)/$', 'linkboard.views.linkboardpage', 
-        name="linkboardpage"), 
+        name="linkboardpage"),
+    url(r'^linkboard/linkpost/$', 'linkboard.views.linkpost', 
+        name="linkpost"),
+
 )
