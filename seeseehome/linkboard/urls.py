7bfbf844dc25a33c69350596119a54759a78b14d
wikibootup <wikibootup@gmail.com>
2014-12-05 00:19:17 +0900

    $ is removed if continuous page does not exist

--- seeseehome/linkboard/urls.py
+++ seeseehome/linkboard/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^linkboard/([0-9]+)/$', 'linkboard.views.linkboardpage', 
+    url(r'^linkboard/([0-9]+)/', 'linkboard.views.linkboardpage', 
         name="linkboardpage"),
-    url(r'^linkboard/linkpost/$', 'linkboard.views.linkpost', 
+    url(r'^linkboard/linkpost/', 'linkboard.views.linkpost', 
         name="linkpost"),
-
 )
