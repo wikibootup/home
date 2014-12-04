63034dff8acbe85b5acc1677badad9bed1a2871c
wikibootup <wikibootup@gmail.com>
2014-12-05 00:17:15 +0900

    board urls.py : $ is removed if continuous url does not exist

--- seeseehome/boards/urls.py
+++ seeseehome/boards/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^boards/([0-9]+)/page/([0-9]+)/$', 'boards.views.boardpage', 
+    url(r'^boards/([0-9]+)/page/([0-9]+)/', 'boards.views.boardpage', 
         name="boardpage"), 
-    url(r'^boards/([0-9]+)/post/([0-9]+)/$', 'boards.views.postpage', 
+    url(r'^boards/([0-9]+)/post/([0-9]+)/', 'boards.views.postpage', 
         name="postpage"),
     url(r'^boards/([0-9]+)/post/$', 'boards.views.write', name="write"),
-    url(r'^boards/([0-9]+)/post/([0-9]+)/rewrite/$', 'boards.views.rewrite', 
+    url(r'^boards/([0-9]+)/post/([0-9]+)/rewrite/', 'boards.views.rewrite', 
         name="rewrite"),
 ) 
