97b9fb8b00d9778f269e38c984de8b351d24666c
wikibootup <wikibootup@gmail.com>
2014-12-05 00:19:26 +0900

    messages added

--- seeseehome/seeseehome/msg.py
+++ seeseehome/seeseehome/msg.py
@@ -26,7 +26,7 @@ users_pwd_no_special_char = "User password must contain special character"
 ### username
 users_name_must_be_set = "User name must be set"
 users_name_at_most_30 = "User name length is limited to 30.\n"
-users_invalid_name = "User name should be alphabet character.\n" +\
+users_invalid_name = "User name should be Korean Character (or Alphabet)\n" +\
                     "And it cannot contain special characters " + \
                     "except for under bar_ , hyphen -"
 
