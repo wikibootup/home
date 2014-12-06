14f4ce08a621c358bb0b4b3696a65182242ca527
wikibootup <wikibootup@gmail.com>
2014-12-06 14:49:45 +0900

    users/tests/test_user_manager.py : regex of special characters for password may be added later

--- seeseehome/users/tests/test_user_manager.py
+++ seeseehome/users/tests/test_user_manager.py
@@ -53,7 +53,8 @@ class UserManagerTestCase(TestCase):
             email = testdata.users_valid_email,
             password = testdata.users_pwd_without_alphabet_char,
         )
-
+    """
+#   regex or special characters may be added later    
     def test_user_password_without_special_char(self):
         self.assertRaises(
             ValidationError,
@@ -62,7 +63,7 @@ class UserManagerTestCase(TestCase):
             email = testdata.users_valid_email,
             password = testdata.users_pwd_without_special_char,
         )
-
+    """
     def test_user_password_under_6_char(self):
          self.assertRaises(
             ValidationError,
