e76f269378374070da2f4f83cd20365a06ce5838
wikibootup <wikibootup@gmail.com>
2014-12-08 02:18:49 +0900

    users/urls.py : editpersonalinfo urlconf added

--- seeseehome/users/urls.py
+++ seeseehome/users/urls.py
@@ -4,7 +4,10 @@ urlpatterns = patterns('',
     url(r'^signup/', 'users.views.signup', name="signup"),
     url(r'^login/', 'users.views.login', name="login"),
     url(r'^logout/', 'users.views.logout', name="logout"),
-    url(r'^personalinfo/', 'users.views.personalinfo', name="personalinfo"),
-    url(r'^editpwd/', 'users.views.editpassword', name="editpwd"),
+    url(r'^personalinfo/$', 'users.views.personalinfo', name="personalinfo"),
+    url(r'^personalinfo/editpersonalinfo/', 'users.views.editpersonalinfo', 
+      name="editpersonalinfo"),
+    url(r'^personalinfo/editpwd/', 'users.views.editpassword', 
+      name="editpwd"),
 )
 
