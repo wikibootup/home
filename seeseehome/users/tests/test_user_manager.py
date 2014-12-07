3ebbfadefea1125dbdd82f692075592fd1aaf602
wikibootup <wikibootup@gmail.com>
2014-12-08 02:18:26 +0900

    users/tests/test_user_manager.py : update user test for contact number added

--- seeseehome/users/tests/test_user_manager.py
+++ seeseehome/users/tests/test_user_manager.py
@@ -152,7 +152,18 @@ class UserManagerTestCase(TestCase):
         updated_user = User.objects.get_user(user.id)
         self.assertEqual(updated_user.userperm, "5")
  
-
+    def test_update_contact_number(self):
+        user = User.objects.create_user(
+                   username = testdata.users_old_name,
+                   email = testdata.users_valid_email,
+                   password = testdata.users_valid_password,
+               )
+        self.assertEqual(user.username, testdata.users_old_name)
+        User.objects.update_user(user.id, 
+                contact_number = testdata.users_contact_number)
+        updated_user = User.objects.get_user(user.id)
+        self.assertEqual(updated_user.contact_number, 
+                testdata.users_contact_number)
 
 ##########
 ##### DELETE
